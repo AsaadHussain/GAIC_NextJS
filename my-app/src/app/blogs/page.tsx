16830f5bcb4e070505fedae29d59d3ae0c5c5d2b
Asaad Hussain <asaadhussain2014@gmail.com>
2024-10-14 15:38:43 +0500

    dynamic rendering

--- my-app/src/app/blogs/page.tsx
+++ my-app/src/app/blogs/page.tsx
@@ -6,12 +6,13 @@ const data = [
         title: "Nature",
         description: "This is nature",
         slug: "nature",
-        src: "./"
+        src: "/nature2.jpg"
     },
     {
         title: "Food",
         description: "All about food",
-        slug: "food"
+        slug: "food",
+        src: "/food2.jpg"
     }
 ]
 
@@ -27,7 +28,7 @@ export default function Blogs() {
                         <Link key={index} href={`/blogs/${x.slug}`}>
                             <div className="card">
                                 <div className="cardImgD">
-                                    <p>img</p>
+                                    <img src={`${x.src}`} alt="image" />
                                 </div>
                                 <div className="circle"></div>
                                 <div className="cardTextD">
